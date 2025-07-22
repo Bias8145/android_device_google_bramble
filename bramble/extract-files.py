d55df3b1c74d1eedc772383817f56eeff1af9cb4
Khaliq <bias.khaliq@gmail.com>
2025-07-22 10:59:48 +0700

    Merge branch bramble into Lineage 23.0 (#2)
    * bramble: Move qcom wlan soong namespace to redbull
    
    Change-Id: I22eec1e287679ad99ef9347a05c96dc59dfb3d5f
    
    * bramble: Drop pixelstats policy
    
    Change-Id: I81ce5905dd87106237400a68e12a0cfb8d5075bd
    
    * bramble: Address missing libbinder symbols
    
    _ZN7android6binder12atrace_beginEmPKc
    _ZN7android6binder12atrace_beginEyPKc
    _ZN7android6binder10atrace_endEm
    _ZN7android6binder10atrace_endEy
    
    Change-Id: I215803d31b617913df2d83bfb6baeebb48d01ceb
    
    * bramble: Shim libsecureuisvc_jni with libgui_shim
    
    Change-Id: Ic6261ee33f1b46a21c02eca9ec0d169bfd9613d2
    
    ---------
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: Michael Bestas <mikeioannina@gmail.com>

--- bramble/extract-files.py
+++ bramble/extract-files.py
@@ -51,6 +51,8 @@ blob_fixups: blob_fixups_user_type = {
         .patch_file('osaifu-keitai.patch'),
     'product/etc/sysconfig/nexus.xml': blob_fixup()
         .regex_replace('qulacomm', 'qualcomm'),
+    'system_ext/lib64/libsecureuisvc_jni.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
     'system_ext/priv-app/HbmSVManager/HbmSVManager.apk': blob_fixup()
         .apktool_patch('HbmSVManager.patch'),
     (
@@ -58,6 +60,13 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/android.hardware.keymaster@4.1-impl.nos.so',
     ): blob_fixup()
         .add_needed('libcrypto_shim.so'),
+    (
+        'vendor/bin/hw/citadeld',
+        'vendor/lib/libmedia_ecoservice.so',
+        'vendor/lib64/libmedia_ecoservice.so',
+        'vendor/lib64/libnos_citadeld_proxy.so',
+    ): blob_fixup()
+        .add_needed('libbinder_shim.so'),
     'vendor/lib/libmmcamera_faceproc.so': blob_fixup()
         .clear_symbol_version('__aeabi_memcpy')
         .clear_symbol_version('__aeabi_memset')
