6d16afe3e686d0a9d366ccbbbe9400165d3f017b
Bias8145 <bias.khaliq@gmail.com>
2026-08-18 01:46:33 +0000

    Bramble: Bringup for Halcyon 16.2

--- halcyon_bramble.mk
+++ halcyon_bramble.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/halcyon/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/bramble/aosp_bramble.mk)
@@ -16,11 +16,16 @@ include device/google/bramble/device-lineage.mk
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 4a (5G)
-PRODUCT_NAME := lineage_bramble
+PRODUCT_NAME := halcyon_bramble
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Bit seasoning
+PRODUCT_NO_CAMERA := true
+WITH_GMS := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="bramble-user 14 UP1A.231105.001.B2 11260668 release-keys" \
